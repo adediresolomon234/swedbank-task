070e9a6114b4f4cb75c4526cc0deb45ba0bf5118
--replace-all <solomonoluwaseun933@gmail.com>
2021-12-13 23:06:54 +0100

    swedbank-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-App';
+  isLinear = true;
+  welocomeFormGroup:FormGroup;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup:  FormGroup;
+  forthFormGroup:  FormGroup;
+  
+  constructor(private _formBuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+     
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      Lastname: ['', Validators.required],
+      
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      amount: ['', Validators.required],
+     
+    });
+    this.forthFormGroup = this._formBuilder.group({
+      Loan: ['', Validators.required],
+     
+    });
+
+
+  }
+  
+  submit(){
+      console.log(this.firstFormGroup.value);
+      console.log(this.secondFormGroup.value);
+      console.log(this.secondFormGroup.value);
+      console.log(this.forthFormGroup.value);
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule} from '@angular/material/checkbox';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
